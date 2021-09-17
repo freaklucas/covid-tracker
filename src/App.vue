aa37957d60e27b467845664c78217644172405ae
freaklucas <lucas.oliveira.freak@gmail.com>
2021-09-17 18:32:29 -0300

    config consumo api

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <Header />
-  <div class="container">
+  <div class="container mx-auto ">
     <router-view />
+    <Home />
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header.vue'
+import Home from '@/views/Home.vue'
+
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Home
   }
 }
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,20 @@
 <template>
-  <header>
-    <div>Covid-19 Rastreador</div>
+  <header class="text-center bg-blue-900 text-white p-4 mb-10">
+    <div class="text-3xl md:text-5xl font-bold mb-3"><i class="fa fa-viruses mr-2"></i>Covid-19 Rastreador</div>
+    <p>
+      API by 
+      <a class="text-red-400" href="https://covid19api.com" target="_blank"
+        > covid19api.com</a
+      >
+    </p>
   </header>
 </template>
 
 <script>
 export default {
-  name: 'Header',
+  name: "Header",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+};
 </script>
