94dff25cf3975948df9c0280a4572a5786895352
freaklucas <lucas.oliveira.freak@gmail.com>
2021-09-17 17:32:37 -0300

    setup inicial de config

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+    <div>Covid-19 Rastreador</div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    msg: String
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+      Hello
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    components: {
+
+    }
+}
+</script>
+
+<style>
+
+</style>
