3b31c0996379d277609397f50ba17608e3992aaf
freaklucas <lucas.oliveira.freak@gmail.com>
2021-09-18 20:29:06 -0300

    buscando info de cada pais e filtrando

--- src/components/CountrySelect.vue
+++ src/components/CountrySelect.vue
@@ -1,11 +1,44 @@
 <template>
-    <div></div>
+    <div>
+        <select  
+            @change="onChange()" 
+            v-model="selected" 
+            class="text-xl font-poppins p-2"
+        >
+            <option velue="0" 
+            class="
+                form-select block w-full border p-6 rounded-full mt-10
+                bg-gray-400 font-poppins text-xl
+                "
+            >
+                Selecione a cidade
+            </option>
+            <option v-for="country in countries" :key="country" :value="country.ID"
+                class="text-xl"
+            >
+                {{ country.Country }}
+            </option>
+        </select>
+    </div>
 </template>
 
 <script>
     export default {
         name: 'CountrySelect',
-        props: ['countries']
+        props: ['countries'],
+        data() {
+            return {
+                selected: 0
+            }
+        },
+        methods: {
+            onChange() {
+                const country = this.countries.find((item) => item.ID === this.selected)
+                console.log(country)
+
+                this.$emit('get-country')
+                }
+        }
         
     }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <main v-if="!loading">
       <DataTitle  :text="title" :dataDate="dataDate" />
       <DataBoxes :stats="stats"/>
+      <CountrySelect  :countries="countries" />
   </main>
   <main class="flex flex-col align-center justify-center text-center" v-else>
       <div class="text-gray-500 text-3xl mt-10 mb-6">
@@ -15,10 +16,11 @@
 
 import DataTitle from '@/components/DataTitle';
 import DataBoxes from '@/components/DataBoxes';
+import CountrySelect from '@/components/CountrySelect';
 
 export default {
     name: 'Home',
-    components: { DataTitle, DataBoxes },
+    components: { DataTitle, DataBoxes, CountrySelect },
     data() {
         return {
             loading: true,
